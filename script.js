c62e80f45371f1393e27b11e359f101695d4e1fd
Cristina.Chifor <cristinachifor93.cc@gmail.com>
2024-03-10 11:06:36 +0200

    Defining variables

--- script.js
+++ script.js
@@ -1,2 +1,25 @@
 let checked = "Hello World";
 console.log(checked);
+
+// variabilele aloca spatiu in memoria browser-ului pentru a stoca date
+// blocuri fundamentale de stocare date in JS
+// 3 metode de a defini variabilele: var- met veche; nu se mai foloseste, let, const
+// conventii de denumire a varibilelor: nume date de noi cat mai reprezentative scrise in camel case ex: numeVariabila
+// JS limbaj de programare sensibil la Majuscule
+// numele variabilelor nu incepe cu cifre si nu se scriu cu spatiu intre primul cuvant si al doilea
+// de evitat abrevierile
+// o var o data declarata poate fi reatribuita
+// o var definita cu let/ var poate fi reatribuita (! in cazul var def cu let nu se mai pune let la var reatribuita)
+// var def cu const nu pot fi reatribuite
+// orice linie de cod in JS se inchide cu ;
+
+let age = 30;
+console.log(age);
+console.log(`My age is ${age}`);
+
+let years = 5;
+console.log(years);
+
+age= 36;
+console.log(age);
+console.log(`My age is ${age}`);
